78bddc58761c848b07d32417de7da0b11830c713
piyush-bhardwaj09 <piyush9453634843bhardwaj@gmail.com>
2023-02-28 14:07:54 +0530

    add some changes becouse of rev comm

--- c++/mul.cpp
+++ c++/mul.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout<<"enter teo digits for multiplication"<<endl;
+    cout<<"enter two digits for multiplication"<<endl;
     cin>>a>>b;
     cout<< a*b << endl;
     return 0;
