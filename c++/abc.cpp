c40d4643363614d3ae09fa74068fae5b6e75e76f
piyush-bhardwaj09 <piyush9453634843bhardwaj@gmail.com>
2023-02-28 13:00:47 +0530

    added new file,program of add

---
+++ c++/abc.cpp
@@ -0,0 +1,32 @@
+// #include(iostream)
+// using namespace std;
+// int main()
+// {
+//     int a,b,c;
+//     cout<<"enter two number's"<<endl;
+//     cin>>a>>b;
+//     c=a+b;
+//     cout<<c<<endl;
+//     return 0;
+// } 
+
+
+#include <iostream>
+// #include <stdlib.h>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+    
+  cout << "Enter two integers: ";
+//   cin >> first_number >> second_number;
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  sum = 15 + 6;
+
+  // prints sum 
+  cout<< sum;     
+
+  return 0;
+}
