c8168f98ec69fbb8f6831671a64531858963042f
piyush-bhardwaj09 <piyush9453634843bhardwaj@gmail.com>
2023-02-28 13:13:20 +0530

    new change added vote

---
+++ c++/aaa.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if (a>b)
+    {
+    cout<<"your are eligble for vote";
+    }
+    else{
+    
+        cout<<"you are not eligble";
+    }
+
+    return 0;
+}
