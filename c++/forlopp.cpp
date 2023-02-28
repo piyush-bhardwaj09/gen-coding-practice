a5515f4a047a465da8aec8de0a42be7c603403c9
piyush-bhardwaj09 <piyush9453634843bhardwaj@gmail.com>
2023-02-28 16:45:10 +0530

    new file added 'forloop'

---
+++ c++/forlopp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int sum=0;
+	 for(int counter=1;counter<=20;counter++)
+	 { sum=sum+counter;
+	 }
+	 cout<<sum<<endl;
+	 return 0;
+}
