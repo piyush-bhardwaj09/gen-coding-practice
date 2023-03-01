63c0eac8a381b9e2b5b75fcf8aaaf023e1520753
piyush-bhardwaj09 <piyush9453634843bhardwaj@gmail.com>
2023-03-01 22:31:09 +0530

    new change

---
+++ c++/eve/eve.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int n1,n2,sum;
+ cout<<" pls enter  two number"<<endl;
+ cin>>n1>>n2;
+ sum=n1+n2;
+ 
+ 
+ if(sum>50)
+ {
+cout<<"great "<<endl;
+ }
+ else
+ {cout<<"not";
+ }
+return 0;
+}
